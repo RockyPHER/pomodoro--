a7c513e9244e68a747d1c2ec9d8284b9917beffe
RockyPHER <davidsonjunior10@gmail.com>
2024-03-22 20:16:43 -0300

    refactor: clock script

--- src/components/clock/main.tsx
+++ src/components/clock/main.tsx
@@ -1,14 +1,24 @@
 import "./style.css";
 import { Play } from "lucide-react";
-import { handleStartTimer } from "./scripts";
+import { changeTime, handleStartTimer, onTimeChange } from "./script";
+import { useEffect, useState } from "react";
 
 export default function Clock() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    const { currentTime, passedTime } = onTimeChange();
+    setCurrentTime(currentTime);
+  }, [changeTime]);
+
   return (
     <section>
       <span></span>
       <button onClick={() => handleStartTimer()}>
         <Play />
       </button>
+      <button>SetCountdown</button>
+      <input type="number"></input>
     </section>
   );
 }
