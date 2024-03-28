6af35ee3e1b62f36c5162a61d4c541494521245c
RockyPHER <davidsonjunior10@gmail.com>
2024-03-27 22:25:09 -0300

    chore: load task button add

--- src/components/clock/buttons/main.tsx
+++ src/components/clock/buttons/main.tsx
@@ -1,21 +1,34 @@
-import { Pause, Play, SkipForward, Square } from "lucide-react";
+import { Pause, Play, SaveAll, SkipForward, Square } from "lucide-react";
 import { useState } from "react";
 import "./style.css";
+import OngoingTask from "../ongoing-task/main";
 
 export default function ClockButtons() {
   const [isPlay, setIsPlay] = useState(false);
+  const [hasTasks, setHasTasks] = useState(false);
 
   return (
-    <div className="clock-button-container">
-      <button className="clock-button skip">
-        <SkipForward />
-      </button>
-      <button className={`clock-button ${isPlay ? "pause" : "play"}`}>
-        {isPlay ? <Pause /> : <Play />}
-      </button>
-      <button className="clock-button stop">
-        <Square />
-      </button>
+    <div className="clock-buttons-container">
+      <div className="clock-top-buttons-container">
+        <button className="clock-button skip">
+          <SkipForward />
+        </button>
+        <button className={`clock-button ${isPlay ? "pause" : "play"}`}>
+          {isPlay ? <Pause /> : <Play />}
+        </button>
+        <button className="clock-button stop">
+          <Square />
+        </button>
+      </div>
+      {!hasTasks ? (
+        <div className="clock-load-container">
+          <button className="load-task-button">
+            <SaveAll className="load-task-icon" />
+          </button>
+        </div>
+      ) : (
+        <OngoingTask />
+      )}
     </div>
   );
 }
---
+++ src/components/clock/ongoing-task/main.tsx
@@ -0,0 +1,3 @@
+export default function OngoingTask() {
+  return <div>OngoingTask</div>;
+}
